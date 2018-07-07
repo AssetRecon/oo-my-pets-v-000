eae3401a55668ae1a54e7d60c56a3b9763403f7a
Edsel Caprice <caprice@mail.usf.edu>
2018-07-07 12:37:08 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,46 @@
 class Owner
-  # code goes here
+attr_accessor :name, :pets
+attr_reader :species
+@@all = []
+
+  def initialize(name)
+    @@all << self
+    @species = "human"
+    @pets = {fishes: [],
+              dogs: [],
+              cats: []
+            }
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+      @@all.clear
+  end
+
+  def self.count
+    self.all.length
+  end
+
+  def species
+    @species
+  end
+
+  def say_species
+    "I am a #{self.species}."
+  end
+
+  def pets
+    @pets
+  end
+
+  def buy_fish(name)
+  new_fish = Fish.new(name)
+  self.pets[fishes] << new_fish
+  end
+
+
+
 end
